2ab0f42ac38d21094b56176b482545aff73c13ed
Yat Cheung <Yat Cheung@localhost>
2017-05-12 14:41:40 +0100

    Code refactored entirely with many modifications to fit MVC model better. New classes created and functionality split to do so. Javadoc added to class files. Unit testing for HTMLParser, JSONBuilder added.

--- Yat/f1resultsparser/src/f1resultsparser/HTMLParser.java
+++ Yat/f1resultsparser/src/f1resultsparser/HTMLParser.java
@@ -6,21 +6,51 @@ import java.net.InetSocketAddress;
 import java.net.Proxy;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /**
  * Class for the parsing of HTML pages allowing for data to be scraped
  * from websites.
  * Requires Jsoup library
+ * 
  * @author Yat Cheung
  *
  */
 
 public class HTMLParser {
+    public Document doc;
+    private String location;
+    private String mode;
+
+ /**
+  * Class Constructor
+  * @param location     a String containing the URL/file path of the HTML file to be parsed
+  * @param mode         a String to indicate whether we are loading from a URL or file path. Accepts only "file" or "url" as argument (case-sensitive)
+  */
+	public HTMLParser(String location, String mode) {
+	    this.setMode(mode);
+	    this.setLocation(location);
+	}
+	
 	/**
-	 * Class constructor
+	 * Method to return a Document object of the HTML file
+	 * @return Document    object of the HTML file
+	 * @throws IOException     if there was an error opening the HTML
+	 * @throws IllegalArgumentException    if mode is anything other than "file" or "url"
 	 */
-	public HTMLParser() {
-		
+	
+	public Document loadHTML() throws IOException
+	{
+
+	    if ("file".equals(mode)) {
+	        doc = loadFromFile(location);
+	    }
+
+	    if ("url".equals(mode)) {
+	        doc = loadFromUrl(location);
+	    }
+
+	    return doc;
 	}
 	
 	/**
@@ -30,7 +60,7 @@ public class HTMLParser {
 	 * @return doc			the Document object of the parsed HTML file
 	 * @throws IOException  if an error occurs whilst loading the file
 	 */
-	Document loadFromFile(String path) throws IOException {
+	public Document loadFromFile(String path) throws IOException {
 		File input = new File(path);
 		return Jsoup.parse(input, "UTF-8");
 	}
@@ -42,10 +72,56 @@ public class HTMLParser {
 	 * @return doc			the Document object of the parsed HTML file
 	 * @throws IOException  if an error occurs whilst fetching the URL
 	 */
-	Document loadFromUrl(String urlString) throws IOException {
+	public Document loadFromUrl(String urlString) throws IOException {
 		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("jdw_websense.jdwilliams.local", 8080));
 
         return Jsoup.connect(urlString).proxy(proxy).get();
 
 	}
+	
+	/**
+	 * Method to select an Element from a Document by name and return it.
+	 * @param elementName  a String of the name of the Element being looked for
+	 * @param from         the Document in which we are looking for the Element
+	 * @return             the first Element encountered in the Document with the provided name
+	 */
+	public Element selectFirstElementByName(String elementName, Document from) throws IllegalArgumentException {
+	    return from.select(elementName).first();
+	}
+	
+	public Element selectFirstElementByName(String elementName, Element from) throws IllegalArgumentException {
+        return from.select(elementName).first();
+    }
+	
+	/**
+     * Method to select an Element from an Element by name and return it.
+     * @param elementName   a String of the name of the Element being looked for
+     * @param from          the Element in which we are looking for the Element
+     * @return              the first Element encountered in the Element with the provided name
+     */
+	
+	/**
+	 * Setter for class constructor
+	 * Ensures only "file" or "url" is accepted as arguments
+	 * @param mode         String mode to set the class' mode field to
+	 * @throws IllegalArgumentException if anything other than "file" or "url" is provided as an argument
+	 */
+	private void setMode(String mode) throws IllegalArgumentException {
+
+	    if ("file".equals(mode) || "url".equals(mode)) {
+	        this.mode = mode;
+	    }
+
+	    else {
+	        throw new IllegalArgumentException();
+	    }
+	}
+
+	
+	
+	private void setLocation(String location) {
+	    this.location = location;
+	}
+	
+	
 }
---
+++ Yat/f1resultsparser/src/f1resultsparser/tests/HTMLParserTest.java
@@ -0,0 +1,136 @@
+package f1resultsparser.tests;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.Before;
+import org.junit.Test;
+
+import f1resultsparser.HTMLParser;
+
+public class HTMLParserTest {
+    HTMLParser hParserModeFile;
+    HTMLParser hParserModeUrl;
+    
+    @Before
+    public void setUp() throws Exception {
+        hParserModeFile = new HTMLParser("src/f1resultsparser/2015F1.html", "file");
+        hParserModeUrl = new HTMLParser("http://www.f1-fansite.com/f1-results/2015-f1-results-standings/", "url");
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testInvalidConstructor() throws IllegalArgumentException {
+        new HTMLParser("src/f1resultsparser/2015F1.html", "fail" );
+    }
+    
+    @Test
+    public void loadHTMLFileTest() throws IOException {
+        hParserModeFile.loadHTML();
+    }
+    
+    @Test
+    public void loadHTMLUrlTest() throws IOException {
+        hParserModeUrl.loadHTML();
+    }
+
+    @Test
+    public void testLoadFromValidFile() throws IOException {
+        hParserModeFile.loadFromFile("src/f1resultsparser/2015F1.html");
+        }
+
+    @Test(expected=FileNotFoundException.class)
+    public void testLoadFromInvalidFile() throws IOException {
+        hParserModeFile.loadFromFile("failcase");
+    }
+
+    @Test
+    public void testLoadFromValidUrl() throws IOException {
+        
+        hParserModeUrl.loadFromUrl("http://www.f1-fansite.com/f1-results/2015-f1-results-standings/");
+
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testLoadFromInvalidUrl() throws IOException {
+        String str = "";
+        hParserModeUrl.loadFromUrl(str);
+    }
+    
+    @Test
+    public void selectFirstElementByNameFromDocValidTest() {
+        Document docTest = new Document("test doc");
+        Element elementTest = docTest.appendElement("testName");
+        
+        Element extractedElem = hParserModeFile.selectFirstElementByName("testName", docTest);
+        
+        assertEquals(elementTest, extractedElem);
+        
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void selectFirstElementByNameFromDocNullCaseTest() {
+        Document docTest = new Document("test doc");
+        docTest.appendElement("testName");
+        
+        hParserModeFile.selectFirstElementByName(null, docTest);
+        
+    }
+    @Test(expected=IllegalArgumentException.class)
+    public void selectFirstElementByNameFromDocEmptyCaseTest() {
+        Document docTest = new Document("test doc");
+        docTest.appendElement("testName");
+        
+        hParserModeFile.selectFirstElementByName("", docTest);
+        
+    }
+    
+    @Test
+    public void selectFirstElementByNameFromDocNotExistTest() {
+        Document docTest = new Document("test doc");
+        docTest.appendElement("testName");
+        
+        assertEquals(hParserModeFile.selectFirstElementByName("testName2", docTest), null);
+        
+    }
+    
+    @Test
+    public void selectFirstElementByNameFromElementValidTest() {
+        Element eleTest = new Element("test ele");
+        Element elementTest = eleTest.appendElement("testName");
+        
+        Element extractedElem = hParserModeFile.selectFirstElementByName("testName", eleTest);
+        
+        assertEquals(elementTest, extractedElem);
+        
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void selectFirstElementByNameFromElementNullCaseTest() {
+        Element eleTest = new Element("test ele");
+        eleTest.appendElement("testName");
+        
+        hParserModeFile.selectFirstElementByName(null, eleTest);
+        
+    }
+    @Test(expected=IllegalArgumentException.class)
+    public void selectFirstElementByNameFromElementEmptyCaseTest() {
+        Element eleTest = new Element("test ele");
+        eleTest.appendElement("testName");
+        
+        hParserModeFile.selectFirstElementByName("", eleTest);
+        
+    }
+    
+    @Test
+    public void selectFirstElementByNameFromElementNotExistTest() {
+        Element eleTest = new Element("test ele");
+        eleTest.appendElement("testName");
+        
+        assertEquals(hParserModeFile.selectFirstElementByName("testName2", eleTest), null);
+        
+    }
+
+}
