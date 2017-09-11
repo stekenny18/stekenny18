876fec25d657c2e51d23decb8f6226e1701f9e8b
Stephen Kenny <stephen.kenny@jdwilliams.co.uk>
2017-09-11 11:43:19 +0100

    F1 graduate training exercise completed using the jsoup library to parse HTML from the website.

---
+++ Ste/MyWorkspace/F1 Assignment/src/Results.java
@@ -0,0 +1,15 @@
+public class Results {
+	
+	String name;
+	String points;
+	
+	public Results(){
+		
+	}
+	
+	public Results(String names, String pointsTotal){
+		
+		 name = names;
+		 points = pointsTotal;
+	}
+}
